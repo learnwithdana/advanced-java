e3607d78eeef5894a93f1268148c9463cea64997
Dana Wyatt <danaatemca@aol.com>
2021-10-27 14:43:30 -0500

    creating hello world project

---
+++ HelloWorld/src/com/hca/hello/MainApp.java
@@ -0,0 +1,17 @@
+package com.hca.hello;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		
+		int y = 3;
+				
+		int z = x + y;
+		
+		System.out.println("z = " + z);
+
+	}
+
+}
